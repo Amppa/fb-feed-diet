ce1397fb4c23297f9a247d4fb64bc4d345221c69
Amppa <7997847+Amppa@users.noreply.github.com>
2026-09-26 00:25:17 +0800

    chore(release): bump version to 2.6.0

--- src/shared/defaults.js
+++ src/shared/defaults.js
@@ -156,7 +156,7 @@
     }
   };
 
-  const EXTENSION_VERSION = '2.5.1';
+  const EXTENSION_VERSION = '2.6.0';
   globalThis.FB_DIET_DEFAULTS = {
     SETTINGS: DEFAULT_SETTINGS,
     COUNTS: DEFAULT_COUNTS,
